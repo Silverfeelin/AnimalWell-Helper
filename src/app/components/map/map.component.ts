9d2ee58347b7a9f94cef4f09fb847e0dffbcf1b4
Silverfeelin <Silverfeelin@gmail.com>
2024-05-26 03:07:35 +0200

    Pink flowers
    Data by brick101 on Discord

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -335,6 +335,7 @@ export class MapComponent implements AfterViewInit, OnDestroy {
     this.markerLayers['chinchilla'] = this.createMarkers(this.markers.chinchilla, 'Chinchilla', 'chinchilla', 'grayscale(100%) brightness(0.7)');
     this.markerLayers['bubbleBird'] = this.createMarkers(this.markers.bubbleBird, 'Bubble Bird', 'bird', 'hue-rotate(40deg) brightness(1.5)');
     this.markerLayers['yellowFlower'] = this.createMarkers(this.markers.yellowFlower, 'Yellow Flower', 'flower-yellow', 'hue-rotate(40deg) brightness(1.2)');
+    this.markerLayers['pinkFlower'] = this.createMarkers(this.markers.pinkFlower, 'Pink Flower', 'flower-pink', 'hue-rotate(350deg) brightness(1.7)');
     this.markerLayers['tulip'] = this.createMarkers(this.markers.tulip, 'Tulip', 'tulip', 'hue-rotate(280deg) brightness(1.7)');
     this.markerLayers['brownMushroom'] = this.createMarkers(this.markers.brownMushroom, 'Brown Mushroom', 'mushroom-brown', 'hue-rotate(40deg) brightness(0.8)');
     this.markerLayers['swirlyPlant'] = this.createMarkers(this.markers.swirlyPlant, 'Swirly Plant', 'plant-swirly', 'hue-rotate(100deg) brightness(1.7)');
